d4133ff7fb41c0ed73253bcb2c3a0d8a4fbc0cb5
Abhishek Vyas <abhishekvyas02032001@gmail.com>
2026-08-18 04:13:23 +0530

    deploy: 2026-08-17 from web/ 7b05397ecdf489fe80edddc65ff54d6c

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
 // is the content hash of the page, written in by the build, and a new build
 // evicts every older cache on activate.
 
-const VERSION = "de2163922353";
+const VERSION = "0af4ca44ba8e";
 const CACHE = `rocky-${VERSION}`;
 const SHELL = ["./", "./rocky.html", "./manifest.json", "./icon.svg"];
 
